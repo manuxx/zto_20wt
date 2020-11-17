740701303de7bd771c6378af335b6ab810c7e227
brinkor <brinkorpl@gmail.com>
2020-11-17 17:14:39 +0100

    trzeci test dziala

--- PetShop/Pet.cs
+++ PetShop/Pet.cs
@@ -2,12 +2,32 @@ using System;
 
 namespace Training.DomainClasses
 {
-    public class Pet 
+    public class Pet : IEquatable<Pet>
     {
         public Sex sex;
         public string name { get; set; }
         public int yearOfBirth { get; set; }
         public float price { get; set; }
         public Species species { get; set; }
+
+        public bool Equals(Pet other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return name == other.name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Pet) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (name != null ? name.GetHashCode() : 0);
+        }
     }
 }
