12f51193c3269208e501515c176b0dfb302dae68
Damian Marek <zmdddamian@gmail.com>
2020-11-17 17:19:09 +0100

    Added functionality for third test

--- PetShop/Pet.cs
+++ PetShop/Pet.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Training.DomainClasses
 {
-    public class Pet 
+    public class Pet : IEquatable<Pet>
     {
+        public bool Equals(Pet other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return name == other.name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Pet) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (name != null ? name.GetHashCode() : 0);
+        }
+
+        public static bool operator ==(Pet left, Pet right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Pet left, Pet right)
+        {
+            return !Equals(left, right);
+        }
+
         public Sex sex;
         public string name { get; set; }
         public int yearOfBirth { get; set; }
