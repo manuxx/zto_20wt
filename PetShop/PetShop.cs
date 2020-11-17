baedc22df9c13278a449696767e15e044ecf403f
Mateusz Karcz <mateusz.karcz@interia.eu>
2020-11-17 16:57:45 +0100

    Zaimplementuj drugi test

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -19,7 +19,10 @@ namespace Training.DomainClasses
 
         public void Add(Pet newPet)
         {
-            _petsInTheStore.Add(newPet);
+            if (!_petsInTheStore.Contains(newPet))
+            {
+                _petsInTheStore.Add(newPet);
+            }
         }
     }
 }
