45b274bbe293612857565596fb855c8bba2ed08a
Aleksander Jarmołkowicz <o.jarmolkowicz@gmail.com>
2020-11-24 18:11:46 +0100

    Filter collection of pets with passed predicates

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -30,17 +30,26 @@ namespace Training.DomainClasses
             sortedPets.Sort((p1,p2) => p1.name.CompareTo(p2.name));
             return sortedPets;
         }
-        public IEnumerable<Pet> AllCats()
+
+        public IEnumerable<Pet> AllCats() => AllWithMatchedPredicate(pet => pet.species == Species.Cat);
+        public IEnumerable<Pet> AllMice() => AllWithMatchedPredicate(pet => pet.species == Species.Mouse);
+        public IEnumerable<Pet> AllFemalePets() => AllWithMatchedPredicate(pet => pet.sex == Sex.Female);
+        public IEnumerable<Pet> AllCatsOrDogs() => AllWithMatchedPredicate(pet => pet.species == Species.Cat || pet.species == Species.Dog);
+        public IEnumerable<Pet> AllPetsButNotMice() => AllWithMatchedPredicate(pet => pet.species != Species.Mouse);
+        public IEnumerable<Pet> AllPetsBornAfter2010() => AllWithMatchedPredicate(pet => pet.yearOfBirth > 2010);
+        public IEnumerable<Pet> AllDogsBornAfter2010() => AllWithMatchedPredicate(pet => pet.species == Species.Dog && pet.yearOfBirth > 2010);
+        public IEnumerable<Pet> AllMaleDogs() => AllWithMatchedPredicate(pet => pet.sex == Sex.Male && pet.species == Species.Dog);
+        public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits() => AllWithMatchedPredicate(pet => pet.yearOfBirth > 2011 || pet.species == Species.Rabbit);
+        private IEnumerable<Pet> AllWithMatchedPredicate(Predicate<Pet> p)
         {
             foreach (var pet in _petsInTheStore)
             {
-                if (pet.species == Species.Cat)
+                if (p.Invoke(pet))
                 {
                     yield return pet;
                 }
             }
         }
-
     }
 
 }
