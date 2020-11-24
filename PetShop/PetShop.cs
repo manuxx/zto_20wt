2cfb517bb781348f2c959e6e5c01e504f2ac39aa
manuxx <manusxyz@gmail.com>
2020-11-24 17:19:45 +0100

    przerobka na typ generyczny

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -27,16 +27,16 @@ namespace Training.DomainClasses
         }
     }
 
-    public class ReadOnlySet : IEnumerable<Pet>
+    public class ReadOnlySet<TItem> : IEnumerable<TItem>
     {
-        private readonly IEnumerable<Pet> _pets;
+        private readonly IEnumerable<TItem> _pets;
 
-        public ReadOnlySet(IEnumerable<Pet> pets)
+        public ReadOnlySet(IEnumerable<TItem> pets)
         {
             _pets = pets;
         }
 
-        public IEnumerator<Pet> GetEnumerator()
+        public IEnumerator<TItem> GetEnumerator()
         {
             return _pets.GetEnumerator();
         }
