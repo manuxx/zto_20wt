5f5de9918eff2ad802c54e973fef08cb57aa48b8
Karol Beresniewicz <karol.beresniewicz@simployer.com>
2020-11-24 17:00:22 +0100

    Make ToImmutableCollection an extension method

---
+++ PetShop/PetShopExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Training.DomainClasses
+{
+    public static class PetShopExtensions
+    {
+        public static IEnumerable<TItem> ToImmutableCollection<TItem>(this IEnumerable<TItem> pets)
+        {
+            foreach (var pet in pets)
+            {
+                yield return pet;
+            }
+        }
+    }
+}
