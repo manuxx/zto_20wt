b56b025cb9c8340923b064d7c6b1e9b09677806e
Karol Beresniewicz <karol.beresniewicz@simployer.com>
2020-11-24 19:41:50 +0100

    Implement ZTOLinq and make use of it :)

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -30,17 +30,16 @@ namespace Training.DomainClasses
             sortedPets.Sort((p1,p2) => p1.name.CompareTo(p2.name));
             return sortedPets;
         }
-        public IEnumerable<Pet> AllCats()
-        {
-            foreach (var pet in _petsInTheStore)
-            {
-                if (pet.species == Species.Cat)
-                {
-                    yield return pet;
-                }
-            }
-        }
 
+
+        public IEnumerable<Pet> AllCats() => _petsInTheStore.That(p => p.species == Species.Cat);
+        public IEnumerable<Pet> AllMice() => _petsInTheStore.That(p => p.species == Species.Mouse);
+        public IEnumerable<Pet> AllFemalePets() => _petsInTheStore.That(p => p.sex == Sex.Female);
+        public IEnumerable<Pet> AllCatsOrDogs() => _petsInTheStore.That(p => p.species.In(Species.Cat, Species.Dog));
+        public IEnumerable<Pet> AllPetsButNotMice() => _petsInTheStore.That(p => p.species != Species.Mouse);
+        public IEnumerable<Pet> AllPetsBornAfter2010() => _petsInTheStore.That(p => p.yearOfBirth > 2010);
+        public IEnumerable<Pet> AllDogsBornAfter2010() => _petsInTheStore.That(p => p.species == Species.Dog && p.yearOfBirth > 2010);
+        public IEnumerable<Pet> AllMaleDogs() => _petsInTheStore.That(p => p.sex == Sex.Male && p.species == Species.Dog);
+        public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits() => _petsInTheStore.That(p => p.yearOfBirth > 2011 || p.species == Species.Rabbit);
     }
-
 }
---
+++ PetShop/ZtoLinq.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Training.DomainClasses
+{
+    public static class ZtoLinq
+    {
+        public static IEnumerable<TItem> That<TItem>(this IEnumerable<TItem> col, Predicate<TItem> condition)
+        {
+            foreach (var item in col)
+            {
+                if (condition(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        private static bool In<TItem>(this TItem searched, IEnumerable<TItem> items) where TItem : class
+        {
+            foreach (var item in items)
+            {
+                if (item == searched)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static bool In<TItem>(this TItem searched, params TItem[] items) where TItem : class => searched.In(items as IEnumerable<TItem>);
+    }
+}
