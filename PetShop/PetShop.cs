2787e15978b322486d32aa0d098b1c6a48edf39b
KacpiM <45212512+KacpiM@users.noreply.github.com>
2020-11-24 17:50:58 +0100

    implemented code for tests including cats and sorting by name

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -24,5 +24,30 @@ namespace Training.DomainClasses
                     return;
             _petsInTheStore.Add(newPet);
         }
+
+        public IEnumerable<Pet> AllCats()
+        {
+            foreach (var pet in _petsInTheStore)
+            {
+                if (pet.species == Species.Cat)
+                    yield return pet;
+            }
+        }
+
+        public IEnumerable<Pet> AllPetsSortedByName()
+        {
+            List<Pet> sortedPetsByName = new List<Pet>(_petsInTheStore);
+            sortedPetsByName.Sort((p1,p2) => p1.name.CompareTo(p2.name));
+
+            return sortedPetsByName;
+        }
+
+        public class PetNameComparer : Comparer<Pet>
+        {
+            public override int Compare(Pet x, Pet y)
+            {
+                return x.name.CompareTo(y.name);
+            }
+        }
     }
 }
