c5028dc154e3efcb73374404b4fa9d7c6d8edf84
Damian Marek <zmdddamian@gmail.com>
2020-12-01 16:42:32 +0100

    zad1

--- PetShop/PetShopExtensions.cs
+++ PetShop/PetShopExtensions.cs
@@ -1,12 +1,28 @@
+using System;
 using System.Collections.Generic;
 
-static internal class PetShopExtensions
+
+namespace Training.DomainClasses
 {
-    public static IEnumerable<TItem> OneAtATime<TItem>(this IEnumerable<TItem> items)
+    static internal class PetShopExtensions
     {
-        foreach (var item in items)
+        public static IEnumerable<TItem> OneAtATime<TItem>(this IEnumerable<TItem> items)
         {
-            yield return item;
+            foreach (var item in items)
+            {
+                yield return item;
+            }
+        }
+
+        public static IEnumerable<TItem> ThatSatisfy<TItem>(this IList<TItem> items, Predicate<TItem> predicate)
+        {
+            foreach (var item in items)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
         }
     }
 }
