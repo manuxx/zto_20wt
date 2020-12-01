0273284400b04e9f203df0989ee937a8e9438fca
manuxx <manusxyz@gmail.com>
2020-12-01 17:46:33 +0100

    porzadki z nazwami

---
+++ PetShop/EnumerableExtensions.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using Training.DomainClasses;
+
+public static class EnumerableExtensions
+{
+    public static IEnumerable<TItem> OneAtATime<TItem>(this IEnumerable<TItem> items)
+    {
+        foreach (var item in items)
+        {
+            yield return item;
+        }
+    }
+
+    public static IEnumerable<TItem> ThatSatisfy<TItem>(this IEnumerable<TItem> items, Predicate<TItem> predicate)
+    {
+        return items.ThatSatisfy(new AnonymousCriteria<TItem>(predicate));
+    }
+
+    public static IEnumerable<TItem> ThatSatisfy<TItem>(this IEnumerable<TItem> items, Criteria<TItem> criteria)
+    {
+        foreach (var item in items)
+        {
+            if (criteria.IsSatisfiedBy(item))
+            {
+                yield return item;
+            }
+        }
+    }
+
+}
