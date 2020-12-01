35997cf950c4e880cf1af9fc4ee1443714677f6b
s171931 <74604252+s171931@users.noreply.github.com>
2020-12-01 17:09:52 +0100

    Move methods to pet

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -34,35 +34,15 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllMice()
             =>
-                _petsInTheStore.ThatSatisfy(IsASpecies(Species.Mouse));
-
-        private static Predicate<Pet> IsASpecies(Species species)
-        {
-            return pet => pet.species == species;
-        }
-
-        private static Predicate<Pet> IsFemale()
-        {
-            return pet => pet.sex == Sex.Female;
-        }
-
-        private static Predicate<Pet> IsBornAfter(int year)
-        {
-            return pet => pet.yearOfBirth > year;
-        }
-
-        private static Predicate<Pet> IsNotASpecies(Species species)
-        {
-            return pet => pet.species != species;
-        }
+                _petsInTheStore.ThatSatisfy(Pet.IsASpecies(Species.Mouse));
 
         public IEnumerable<Pet> AllCats()
             =>
-                _petsInTheStore.ThatSatisfy(IsASpecies(Species.Cat));
+                _petsInTheStore.ThatSatisfy(Pet.IsASpecies(Species.Cat));
 
         public IEnumerable<Pet> AllFemalePets()
             =>
-                _petsInTheStore.ThatSatisfy(IsFemale());
+                _petsInTheStore.ThatSatisfy(Pet.IsFemale());
 
         public IEnumerable<Pet> AllCatsOrDogs()
             =>
@@ -70,7 +50,7 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllPetsButNotMice()
             =>
-                _petsInTheStore.ThatSatisfy(IsNotASpecies(Species.Mouse));
+                _petsInTheStore.ThatSatisfy(Pet.IsNotASpecies(Species.Mouse));
 
         public IEnumerable<Pet> AllMaleDogs()
             =>
@@ -78,7 +58,7 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllPetsBornAfter2010()
             =>
-                _petsInTheStore.ThatSatisfy(IsBornAfter(2010));
+                _petsInTheStore.ThatSatisfy(Pet.IsBornAfter(2010));
 
         public IEnumerable<Pet> AllDogsBornAfter2010()
             =>
