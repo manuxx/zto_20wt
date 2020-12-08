03c8bbf1620bc56f7652755c2b12ce23fa8adf1a
Mateusz <mateuszkolakowski98@gmail.com>
2020-12-08 16:45:27 +0100

    zad1

--- PetShop/Pet.cs
+++ PetShop/Pet.cs
@@ -104,5 +104,19 @@ namespace Training.DomainClasses
         }
     }
 
-    
+    public class Negation<TItem> : Criteria<TItem>
+    {
+        private readonly Criteria<TItem> _criteriaForNegation;
+
+        public Negation(Criteria<TItem> criteriaForNegation)
+        {
+            _criteriaForNegation = criteriaForNegation;
+            throw new NotImplementedException();
+        }
+
+        public bool IsSatisfiedBy(TItem item)
+        {
+            return ! _criteriaForNegation.IsSatisfiedBy(item);
+        }
+    }
 }
