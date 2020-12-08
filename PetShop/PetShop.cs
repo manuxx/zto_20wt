27030ce152637637e5f1d1a85dbc71d685fc6db4
Mateusz <mateuszkolakowski98@gmail.com>
2020-12-08 16:52:44 +0100

    zad2

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -5,70 +5,94 @@ namespace Training.DomainClasses
 {
     public class PetShop
     {
-        private IList<Pet> _petsInTheStore;
+        private IList<TItem> _petsInTheStore;
 
-        public PetShop(IList<Pet> petsInTheStore)
+        public PetShop(IList<TItem> petsInTheStore)
         {
             this._petsInTheStore = petsInTheStore;
         }
 
-        public IEnumerable<Pet> AllPets()
+        public IEnumerable<TItem> AllPets()
         {
-            return new ReadOnly<Pet>(_petsInTheStore);
+            return new ReadOnly<TItem>(_petsInTheStore);
         }
 
-        public void Add(Pet newPet)
+        public void Add(TItem newItem)
         {
             foreach (var pet in _petsInTheStore)
-                if (pet.name == newPet.name)
+                if (pet.name == newItem.name)
                     return;
-            _petsInTheStore.Add(newPet);
+            _petsInTheStore.Add(newItem);
         }
 
-        public IEnumerable<Pet> AllPetsSortedByName()
+        public IEnumerable<TItem> AllPetsSortedByName()
         {
-            List<Pet> sortedPets = new List<Pet>(_petsInTheStore);
+            List<TItem> sortedPets = new List<TItem>(_petsInTheStore);
             sortedPets.Sort((p1,p2) => p1.name.CompareTo(p2.name));
             return sortedPets;
         }
 
-        public IEnumerable<Pet> AllMice()
+        public IEnumerable<TItem> AllMice()
             =>
-                _petsInTheStore.ThatSatisfy(Pet.IsASpecies(Species.Mouse));
+                _petsInTheStore.ThatSatisfy(TItem.IsASpecies(Species.Mouse));
 
-        public IEnumerable<Pet> AllPetsBornAfter2010()
+        public IEnumerable<TItem> AllPetsBornAfter2010()
             =>
-                _petsInTheStore.ThatSatisfy(Pet.IsBornAfter(2010));
+                _petsInTheStore.ThatSatisfy(TItem.IsBornAfter(2010));
 
-        public IEnumerable<Pet> AllCats()
+        public IEnumerable<TItem> AllCats()
             =>
-                _petsInTheStore.ThatSatisfy(Pet.IsASpecies(Species.Cat));
+                _petsInTheStore.ThatSatisfy(TItem.IsASpecies(Species.Cat));
 
-        public IEnumerable<Pet> AllFemalePets()
+        public IEnumerable<TItem> AllFemalePets()
             =>
-                _petsInTheStore.ThatSatisfy(Pet.IsFemale());
+                _petsInTheStore.ThatSatisfy(TItem.IsFemale());
 
-        public IEnumerable<Pet> AllCatsOrDogs()
+        public IEnumerable<TItem> AllCatsOrDogs()
             =>
+<<<<<<< Updated upstream
                 _petsInTheStore.ThatSatisfy((pet => pet.species == Species.Dog || pet.species == Species.Cat));
+=======
+                _petsInTheStore.ThatSatisfy(new Alternative(TItem.IsASpecies(Species.Dog),  TItem.IsASpecies(Species.Cat)));
+>>>>>>> Stashed changes
 
-        public IEnumerable<Pet> AllPetsButNotMice()
+        public IEnumerable<TItem> AllPetsButNotMice()
             =>
+<<<<<<< Updated upstream
                 _petsInTheStore.ThatSatisfy(Pet.IsNotASpecies(Species.Mouse));
+=======
+                _petsInTheStore.ThatSatisfy(new Negation<TItem>(TItem.IsASpecies(Species.Mouse)));
+>>>>>>> Stashed changes
 
-        public IEnumerable<Pet> AllMaleDogs()
+        public IEnumerable<TItem> AllMaleDogs()
             =>
                 _petsInTheStore.ThatSatisfy((pet => pet.species == Species.Dog && pet.sex == Sex.Male));
 
 
-        public IEnumerable<Pet> AllDogsBornAfter2010()
+        public IEnumerable<TItem> AllDogsBornAfter2010()
             =>
                 _petsInTheStore.ThatSatisfy((pet => pet.yearOfBirth > 2010 && pet.species == Species.Dog));
 
-        public IEnumerable<Pet> AllPetsBornAfter2011OrRabbits()
+        public IEnumerable<TItem> AllPetsBornAfter2011OrRabbits()
             =>
                 _petsInTheStore.ThatSatisfy((pet => pet.yearOfBirth > 2011 || pet.species == Species.Rabbit));
 
     }
 
+    public class Alternative : Criteria<TItem>
+    {
+        private Criteria<TItem> _criteria1;
+        private Criteria<TItem> _criteria2;
+
+        public Alternative(Criteria<TItem> criteria1, Criteria<TItem> criteria2)
+        {
+            _criteria2 = criteria2;
+            _criteria1 = criteria1;
+        }
+
+        public bool IsSatisfiedBy(TItem item)
+        {
+            return _criteria1.IsSatisfiedBy(item) || _criteria2.IsSatisfiedBy(item);
+        }
+    }
 }
--- PetShopSpec/PetShopSpec.cs
+++ PetShopSpec/PetShopSpec.cs
@@ -11,16 +11,16 @@ namespace Training.Spec
     {
         Establish context = () =>
         {
-            pet_initial_content = new List<Pet>();
+            pet_initial_content = new List<TItem>();
             ProvideBasicConstructorArgument(pet_initial_content);
         }; 
-        protected static IList<Pet> pet_initial_content;
+        protected static IList<TItem> pet_initial_content;
     }
     
     [Subject(typeof(PetShop))]
     public class when_counting_pets_in_the_shop : pet_shop_concern
     {
-        Establish context = () => pet_initial_content.AddManyItems(new Pet(), new Pet()); 
+        Establish context = () => pet_initial_content.AddManyItems(new TItem(), new TItem()); 
         Because of = () => number_of_pets = subject.AllPets().CountItems(); 
         private static int number_of_pets;
         It should_return_the_number_of_all_pets_in_the_shop = () => 
@@ -32,29 +32,29 @@ namespace Training.Spec
     {
         Establish context = () => 
         {
-            first_pet = new Pet();
-            second_pet = new Pet();
-            pet_initial_content.AddManyItems(first_pet, second_pet);
+            _firstItem = new TItem();
+            _secondItem = new TItem();
+            pet_initial_content.AddManyItems(_firstItem, _secondItem);
         };
         Because of = () => pets_in_shop = subject.AllPets();
         It should_return_all_the_pets_in_the_shop = () => 
-            pet_initial_content.ShouldContainOnly(first_pet, second_pet);
+            pet_initial_content.ShouldContainOnly(_firstItem, _secondItem);
 
-        private static Pet first_pet;
-        private static Pet second_pet;
-        private static IEnumerable<Pet> pets_in_shop;
+        private static TItem _firstItem;
+        private static TItem _secondItem;
+        private static IEnumerable<TItem> pets_in_shop;
     }
 
     [Subject(typeof(PetShop))]
     public class when_adding_a_new_pet : pet_shop_concern
     {
-        Establish context = () => pet = new Pet();
-        Because of = () => subject.Add(pet);
+        Establish context = () => _item = new TItem();
+        Because of = () => subject.Add(_item);
 
         It should_store_a_new_pet_in_the_shop = () => 
-            subject.AllPets().ShouldContain(pet);
+            subject.AllPets().ShouldContain(_item);
 
-        private static Pet pet;
+        private static TItem _item;
     }
 
     [Subject(typeof(PetShop))]
@@ -62,16 +62,16 @@ namespace Training.Spec
     {
         Establish context = () =>
         {
-            pet = new Pet();
-            pet_initial_content.Add(pet);
+            _item = new TItem();
+            pet_initial_content.Add(_item);
         };
         Because of = () => 
-            subject.Add(pet);
+            subject.Add(_item);
 
         It should_store_a_pet_in_the_shop_once = () => 
             subject.AllPets().CountItems().ShouldEqual(1);
 
-        private static Pet pet;
+        private static TItem _item;
     }
 
 
@@ -80,28 +80,28 @@ namespace Training.Spec
     {
         Establish context = () =>
                           {
-                              fluffy_the_first = new Pet {name = "Fluffy"};
-                              fluffy_the_second = new Pet {name = "Fluffy"};
+                              fluffy_the_first = new TItem {name = "Fluffy"};
+                              fluffy_the_second = new TItem {name = "Fluffy"};
                               pet_initial_content.Add(fluffy_the_first);
                           };
         Because of = () => subject.Add(fluffy_the_second);
         It should_contain_only_one_pet_of_the_name_in_the_store = () => 
             subject.AllPets().CountItems().ShouldEqual(1);
 
-        private static Pet fluffy_the_first;
-        private static Pet fluffy_the_second;
+        private static TItem fluffy_the_first;
+        private static TItem fluffy_the_second;
     }
 
     [Subject(typeof(PetShop))]
     class when_trying_to_change_returned_collection_of_pets : pet_shop_concern
     {
-        Establish c = () => pet_initial_content.AddManyItems(new Pet { name = "Pixie" }, new Pet { name = "Dixie" });
+        Establish c = () => pet_initial_content.AddManyItems(new TItem { name = "Pixie" }, new TItem { name = "Dixie" });
         Because b = () =>
         {
-            IEnumerable<Pet> returned_pets = subject.AllPets();
-            exception = Catch.Exception(() => { var x = (ICollection<Pet>)returned_pets; });
+            IEnumerable<TItem> returned_pets = subject.AllPets();
+            exception = Catch.Exception(() => { var x = (ICollection<TItem>)returned_pets; });
         };
-        private static IEnumerable<Pet> returned_collection_of_pets;
+        private static IEnumerable<TItem> returned_collection_of_pets;
         private static Exception exception;
         It invalid_cast_exception_should_be_thrown = () => exception.ShouldBeOfExactType<InvalidCastException>();
     }
@@ -110,7 +110,7 @@ namespace Training.Spec
     {
         private Establish c = () =>
         {
-            mouse_Dixie = new Pet
+            mouse_Dixie = new TItem
             {
                 name = "Dixie",
                 species = Species.Mouse,
@@ -118,7 +118,7 @@ namespace Training.Spec
                 sex = Sex.Female,
                 yearOfBirth = 2011
             };
-            mouse_Jerry = new Pet
+            mouse_Jerry = new TItem
             {
                 name = "Jerry",
                 species = Species.Mouse,
@@ -127,7 +127,7 @@ namespace Training.Spec
                 yearOfBirth = 2012
             };
 
-            cat_Tom = new Pet
+            cat_Tom = new TItem
             {
                 name = "Tom",
                 species = Species.Cat,
@@ -135,7 +135,7 @@ namespace Training.Spec
                 sex = Sex.Male,
                 yearOfBirth = 2010
             };
-            cat_Jinx = new Pet
+            cat_Jinx = new TItem
             {
                 name = "Jinx",
                 species = Species.Cat,
@@ -143,7 +143,7 @@ namespace Training.Spec
                 sex = Sex.Male,
                 yearOfBirth = 2009
             };
-            rabbit_Fluffy = new Pet
+            rabbit_Fluffy = new TItem
             {
                 name = "Fluffy",
                 species = Species.Rabbit,
@@ -151,7 +151,7 @@ namespace Training.Spec
                 sex = Sex.Male,
                 yearOfBirth = 2011
             };
-            dog_Huckelberry = new Pet
+            dog_Huckelberry = new TItem
             {
                 name = "Huckelberry",
                 species = Species.Dog,
@@ -159,7 +159,7 @@ namespace Training.Spec
                 sex = Sex.Male,
                 yearOfBirth = 2008
             };
-            dog_Lassie = new Pet
+            dog_Lassie = new TItem
             {
                 name = "Lassie",
                 species = Species.Dog,
@@ -167,7 +167,7 @@ namespace Training.Spec
                 sex = Sex.Female,
                 yearOfBirth = 2007
             };
-            dog_Pluto = new Pet
+            dog_Pluto = new TItem
             {
                 name = "Pluto",
                 species = Species.Dog,
@@ -185,14 +185,14 @@ namespace Training.Spec
                                              mouse_Jerry);
         };
 
-        protected static Pet mouse_Dixie;
-        protected static Pet mouse_Jerry;
-        protected static Pet rabbit_Fluffy;
-        protected static Pet cat_Jinx;
-        protected static Pet cat_Tom;
-        protected static Pet dog_Huckelberry;
-        protected static Pet dog_Lassie;
-        protected static Pet dog_Pluto;
+        protected static TItem mouse_Dixie;
+        protected static TItem mouse_Jerry;
+        protected static TItem rabbit_Fluffy;
+        protected static TItem cat_Jinx;
+        protected static TItem cat_Tom;
+        protected static TItem dog_Huckelberry;
+        protected static TItem dog_Lassie;
+        protected static TItem dog_Pluto;
     }
 
     public class when_searching_for_pets : concern_with_pets_for_sorting_and_filtering
