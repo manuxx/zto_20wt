a0cab1c09ca9cd97a4380c0c42de5bfc6411b3f8
s171931 <74604252+s171931@users.noreply.github.com>
2020-12-08 18:32:42 +0100

    CriteriaBuilder

---
+++ PetShop/CriteriaBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Training.Spec
+{
+    public class CriteriaBuilder<TItem,TProperties>
+    {
+        private readonly Func<TItem, TProperties> _propertySelector;
+
+        public CriteriaBuilder(Func<TItem, TProperties> propertySelector)
+        {
+            _propertySelector = propertySelector;
+        }
+
+        public Criteria<TItem> EqualTo(TProperties mouse)
+        {
+            return new AnonymousCriteria<TItem>(p=>_propertySelector(p).Equals(mouse));
+        }
+    }
+}
