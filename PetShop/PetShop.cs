8b384f1444c7a557642a17b9816db7f9745df3f0
Mateusz Karcz <mateusz.karcz@interia.eu>
2020-12-08 17:00:48 +0100

    8 gru: Koniunkcja

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 
 namespace Training.DomainClasses
@@ -28,7 +27,7 @@ namespace Training.DomainClasses
         public IEnumerable<Pet> AllPetsSortedByName()
         {
             List<Pet> sortedPets = new List<Pet>(_petsInTheStore);
-            sortedPets.Sort((p1,p2) => p1.name.CompareTo(p2.name));
+            sortedPets.Sort((p1, p2) => p1.name.CompareTo(p2.name));
             return sortedPets;
         }
 
@@ -50,7 +49,7 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllCatsOrDogs()
             =>
-                _petsInTheStore.ThatSatisfy(new Alternative<Pet>(Pet.IsASpecies(Species.Dog),  Pet.IsASpecies(Species.Cat)));
+                _petsInTheStore.ThatSatisfy(new Alternative<Pet>(Pet.IsASpecies(Species.Dog), Pet.IsASpecies(Species.Cat)));
 
         public IEnumerable<Pet> AllPetsButNotMice()
             =>
@@ -58,8 +57,7 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllMaleDogs()
             =>
-                _petsInTheStore.ThatSatisfy((pet => pet.species == Species.Dog && pet.sex == Sex.Male));
-
+                _petsInTheStore.ThatSatisfy(new Conjunction<Pet>(Pet.IsASpecies(Species.Dog), Pet.IsMale()));
 
         public IEnumerable<Pet> AllDogsBornAfter2010()
             =>
@@ -69,5 +67,22 @@ namespace Training.DomainClasses
             =>
                 _petsInTheStore.ThatSatisfy((pet => pet.yearOfBirth > 2011 || pet.species == Species.Rabbit));
 
+    }
+
+    public class Conjunction<TItem> : Criteria<TItem>
+    {
+        private readonly Criteria<TItem> m_criteria1;
+        private readonly Criteria<TItem> m_criteria2;
+
+        public Conjunction(Criteria<TItem> criteria1, Criteria<TItem> criteria2)
+        {
+            m_criteria1 = criteria1;
+            m_criteria2 = criteria2;
+        }
+
+        public bool IsSatisfiedBy(TItem item)
+        {
+            return m_criteria1.IsSatisfiedBy(item) && m_criteria2.IsSatisfiedBy(item);
+        }
     }
 }
