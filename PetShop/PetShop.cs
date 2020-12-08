2f872e1be884f3254f6c452328eb3e8ffdc28980
pfroelke <patryk.froelke@gmail.com>
2020-12-08 18:13:50 +0100

    add own LINQlike DSL

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -31,10 +31,6 @@ namespace Training.DomainClasses
             return sortedPets;
         }
 
-        public IEnumerable<Pet> AllMice()
-            =>
-                _petsInTheStore.ThatSatisfy(Pet.IsASpecies(Species.Mouse));
-
         public IEnumerable<Pet> AllPetsBornAfter2010()
             =>
                 _petsInTheStore.ThatSatisfy(Pet.IsBornAfter(2010));
---
+++ PetShop/Where.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Training.DomainClasses
+{
+    public static class Where<TItem>
+    {
+        public static CriteriaBuilder<TItem, TProperties> HasAn<TProperties>(Func<TItem, TProperties> propertySelector)
+        {
+            return new CriteriaBuilder<TItem, TProperties>(propertySelector);
+        }
+
+        public class CriteriaBuilder<TItem, TProperties>
+        {
+            private readonly Func<TItem, TProperties> _propertySelector;
+
+            public CriteriaBuilder(Func<TItem, TProperties> propertySelector)
+            {
+                _propertySelector = propertySelector;
+            }
+
+            public Criteria<TItem> EqualTo(TProperties value)
+            {
+                return new AnonymousCriteria<TItem>(p=>_propertySelector(p).Equals(value));
+            }
+        }
+    }
+}
