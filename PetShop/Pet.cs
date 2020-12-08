97934549dc397615175be76e5928548bf628862f
s171931 <74604252+s171931@users.noreply.github.com>
2020-12-08 16:50:00 +0100

    Alternative

--- PetShop/Pet.cs
+++ PetShop/Pet.cs
@@ -75,6 +75,23 @@ namespace Training.DomainClasses
             }
         }
 
+        public class Alternative<TItem> : Criteria<TItem>
+        {
+            private readonly Criteria<TItem> _firstcriteria;
+            private readonly Criteria<TItem> _secondcriteria;
+
+            public Alternative(Criteria<TItem> firstcriteria, Criteria<TItem> secondcriteria)
+            {
+                _firstcriteria = firstcriteria;
+                _secondcriteria = secondcriteria;
+            }
+
+            public bool IsSatisfiedBy(TItem item)
+            {
+                return _firstcriteria.IsSatisfiedBy(item)||_secondcriteria.IsSatisfiedBy(item);
+            }
+        }
+
         public class SpeciesCriteria : Criteria<Pet>
         {
             private readonly Species _species;
