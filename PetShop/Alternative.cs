a5d6605ab0572f28f9aa94332f79e2050177c976
RudylubiPC <alternatywny.rudy@gmail.com>
2020-12-08 17:27:00 +0100

    BinaryCriteriaAdded

--- PetShop/Alternative.cs
+++ PetShop/Alternative.cs
@@ -1,17 +1,12 @@
 namespace Training.DomainClasses
 {
-    public class Alternative<TITem> : Criteria<TITem>
+    public class Alternative<TITem> : BinaryCriteria<TITem>
     {
-        private readonly Criteria<TITem> _firstCriteria;
-        private readonly Criteria<TITem> _secondCriteria;
-
-        public Alternative(Criteria<TITem> firstCriteria, Criteria<TITem> secondCriteria)
+        public Alternative(Criteria<TITem> firstCriteria, Criteria<TITem> secondCriteria) : base(firstCriteria, secondCriteria)
         {
-            _firstCriteria = firstCriteria;
-            _secondCriteria = secondCriteria;
         }
 
-        public bool IsSatisfiedBy(TITem item)
+        public override bool IsSatisfiedBy(TITem item)
         {
             return _firstCriteria.IsSatisfiedBy(item) || _secondCriteria.IsSatisfiedBy(item);
         }
--- PetShop/Conjunction.cs
+++ PetShop/Conjunction.cs
@@ -1,17 +1,12 @@
 namespace Training.DomainClasses
 {
-    public class Conjunction<TItem> : Criteria<TItem>
+    public class Conjunction<TItem> : BinaryCriteria<TItem>
     {
-        private readonly Criteria<TItem> _firstCriteria;
-        private readonly Criteria<TItem> _secondCriteria;
-
-        public Conjunction(Criteria<TItem> firstCriteria, Criteria<TItem> secondCriteria)
+        public Conjunction(Criteria<TItem> firstCriteria, Criteria<TItem> secondCriteria) : base(firstCriteria, secondCriteria)
         {
-            _firstCriteria = firstCriteria;
-            _secondCriteria = secondCriteria;
         }
 
-        public bool IsSatisfiedBy(TItem item)
+        public override bool IsSatisfiedBy(TItem item)
         {
             return _firstCriteria.IsSatisfiedBy(item) && _secondCriteria.IsSatisfiedBy(item);
         }
