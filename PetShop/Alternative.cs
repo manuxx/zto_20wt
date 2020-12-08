ab26ce2c97e11fa3b2e13fe4e25d77fa1de52c21
Michał Góraj <s171937@student.pg.edu.pl>
2020-12-08 16:54:07 +0100

    zad2

---
+++ PetShop/Alternative.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Training.DomainClasses
+{
+    public class Alternative : Criteria<TItem>
+    {
+        private readonly Criteria<TItem> _leftCriteria;
+        private readonly Criteria<TItem> _rightCriteria;
+
+        public Alternative(Criteria<TItem> leftCriteria, Criteria<TItem> rightCriteria)
+        {
+            _leftCriteria = leftCriteria;
+            _rightCriteria = rightCriteria;
+        }
+
+        public bool IsSatisfiedBy(TItem item)
+        {
+            return _leftCriteria.IsSatisfiedBy(item) || _rightCriteria.IsSatisfiedBy(item);
+            throw new NotImplementedException();
+        }
+    }
+}
