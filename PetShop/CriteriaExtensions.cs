9f3d08ba401314da95aca7ea0f38f00d72a575d5
FFugi <fudifil@gmail.com>
2020-12-08 17:16:29 +0100

    Implement And

---
+++ PetShop/CriteriaExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Training.DomainClasses;
+
+
+public static class CriteriaExtensions
+{
+    public static Criteria<TItem> And<TItem>(this Criteria<TItem> lhsCriteria, Criteria<TItem> rhsCriteria)
+    {
+        return new Conjunction<TItem>(lhsCriteria, rhsCriteria);
+    }
+}
