b72a5e19db62a5e02ccab13eeec7b556039e4ae3
Michał Góraj <s171937@student.pg.edu.pl>
2020-12-08 19:51:49 +0100

    koncowe zad

---
+++ PetShopSpec/CriteriaBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Training.Spec
+{
+    public class CriteriaBuilder<TItem, TProperties>
+    {
+        private readonly Func<TItem, TProperties> _propertySelector;
+
+        public CriteriaBuilder(Func<TItem, TProperties> propertySelector)
+        {
+            _propertySelector = propertySelector;
+        }
+        public Criteria<TItem> EqualTo(TProperties propertyToCompare)
+        {
+            return new AnonymousCriteria<TItem>(p => _propertySelector(p).Equals(propertyToCompare));
+        }
+    }
+}
---
+++ PetShopSpec/Where.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Training.Spec
+{
+    public class Where<TItem>
+    {
+        public static CriteriaBuilder<TItem, TProperty> hasAn<TProperty>(Func<TItem, TProperty> propertySelector)
+        {
+            return new CriteriaBuilder<TItem, TProperty>(propertySelector);
+        }
+    }
+}
