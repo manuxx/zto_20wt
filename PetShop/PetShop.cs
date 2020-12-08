193ea5051bf6b87c0f254e1278a0a192c51f2f8b
Aleksander Jarmołkowicz <o.jarmolkowicz@gmail.com>
2020-12-08 16:51:41 +0100

    Alternative implementation

--- PetShop/PetShop.cs
+++ PetShop/PetShop.cs
@@ -50,7 +50,7 @@ namespace Training.DomainClasses
 
         public IEnumerable<Pet> AllCatsOrDogs()
             =>
-                _petsInTheStore.ThatSatisfy(new Alternative(Pet.IsASpecies(Species.Dog),  Pet.IsASpecies(Species.Cat)));
+                _petsInTheStore.ThatSatisfy(new Alternative<Pet>(Pet.IsASpecies(Species.Dog),  Pet.IsASpecies(Species.Cat)));
 
         public IEnumerable<Pet> AllPetsButNotMice()
             =>
@@ -71,4 +71,18 @@ namespace Training.DomainClasses
 
     }
 
+    public class Alternative<TItem> : Criteria<TItem>
+    {
+        private readonly Criteria<TItem> _firstCriteria;
+        private readonly Criteria<TItem> _secondCriteria;
+        public Alternative(Criteria<TItem> firstCriteria, Criteria<TItem> secondCriteria)
+        {
+            _firstCriteria = firstCriteria;
+            _secondCriteria = secondCriteria;
+        }
+        public bool IsSatisfiedBy(TItem item)
+        {
+            return _firstCriteria.IsSatisfiedBy(item) || _secondCriteria.IsSatisfiedBy(item);
+        }
+    }
 }
