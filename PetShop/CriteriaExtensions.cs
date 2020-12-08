d630d4318ef8be66667584872379b9b44ee64b5c
szymonzygzula <zygzula.szymon@gmail.com>
2020-12-08 17:33:12 +0100

    Dodanie rozszerzenie Or

--- PetShop/CriteriaExtensions.cs
+++ PetShop/CriteriaExtensions.cs
@@ -9,4 +9,8 @@ public static class CriteriaExtensions
         return new Conjunction<TItem>(criteria1, criteria2);
     }
 
+    public static Criteria<TItem> Or<TItem>(this Criteria<TItem> criteria1, Criteria<TItem> criteria2)
+    {
+        return new Alternative<TItem>(criteria1, criteria2);
+    }
 }
