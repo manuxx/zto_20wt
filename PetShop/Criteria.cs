3e94733a384e2bcdfb02c14a143ed363d12022bf
FFugi <fudifil@gmail.com>
2020-12-08 16:39:36 +0100

    Implement negation

--- PetShop/Criteria.cs
+++ PetShop/Criteria.cs
@@ -2,3 +2,17 @@ public interface Criteria<T>
 {
     bool IsSatisfiedBy(T pet);
 }
+
+public class Negation<T> : Criteria<T>
+{
+    private readonly Criteria<T> _baseCriteria;
+    public Negation(Criteria<T> criteria)
+    {
+        _baseCriteria = criteria;
+    }
+
+    public bool IsSatisfiedBy(T pet)
+    {
+        return !_baseCriteria.IsSatisfiedBy(pet);
+    }
+}
