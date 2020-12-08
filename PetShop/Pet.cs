c7746e208359ba819bcf35e0e47939a7eed8226c
manuxx <manusxyz@gmail.com>
2020-12-08 16:40:33 +0100

    rozwiazanie

--- PetShop/Pet.cs
+++ PetShop/Pet.cs
@@ -104,5 +104,18 @@ namespace Training.DomainClasses
         }
     }
 
-    
+    public class Negation<TItem> : Criteria<TItem>
+    {
+        private readonly Criteria<TItem> _criteria4Negation;
+
+        public Negation(Criteria<TItem> criteria4negation)
+        {
+            _criteria4Negation = criteria4negation;
+        }
+
+        public bool IsSatisfiedBy(TItem item)
+        {
+            return ! _criteria4Negation.IsSatisfiedBy(item);
+        }
+    }
 }
