4851ec6237312cfb62ff9045a4218dc801471688
szymonzygzula <zygzula.szymon@gmail.com>
2020-12-08 16:40:51 +0100

    Dodanie klasy Negation

--- PetShop/Pet.cs
+++ PetShop/Pet.cs
@@ -104,5 +104,18 @@ namespace Training.DomainClasses
         }
     }
 
-    
+    public class Negation<TItem> : Criteria<TItem>
+    {
+        private Criteria<TItem> _isSatisfiedCriteria;
+
+        public Negation(Criteria<TItem> isSatisfiedCriteria)
+        {
+            _isSatisfiedCriteria = isSatisfiedCriteria;
+        }
+
+        public bool IsSatisfiedBy(TItem item)
+        {
+            return !_isSatisfiedCriteria.IsSatisfiedBy(item);
+        }
+    }
 }
